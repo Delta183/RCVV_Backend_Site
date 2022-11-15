540162bd97da0b23dc655180a66b41188f3fb1e4
Daniel Figueroa <danfigueroa81@gmail.com>
2022-11-15 17:28:54 -0500

    import sidebar and make router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Sidebar from "./components/Sidebar.js";
+// Router being imported as a means to navigate all the pages
+import { BrowserRouter as Router} from "react-router-dom";
 
+// This file structure is curious but more current
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        // Wrap the entire app in the Router tags
+        <Router>
+            <div className="App">
+                <Sidebar />
+            </div>
+        </Router>
     );
 };
 
