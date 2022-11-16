55b8b3d12305d4f697a571bd7d0974c563c53569
Daniel Figueroa <danfigueroa81@gmail.com>
2022-11-16 03:29:59 -0500

    implement article data into each card dynamically

--- client/src/components/ArticleComponents/Articles/Article/Article.js
+++ client/src/components/ArticleComponents/Articles/Article/Article.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import moment from 'moment';
+import { useDispatch } from 'react-redux';
 
-const Article = () => {
+const Article = ({ article, setCurrentId }) => {
+    const dispatch = useDispatch();
+
     return (
         // The card subheaders may need ids
         <Card style={{ width: '18rem' }}>
-            {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+            <Card.Img variant="top" src={article.selectedFile} /> 
             <Card.Body>
-            <Card.Title>Title</Card.Title>
-            <Card.Subtitle>Author</Card.Subtitle>
-            <Card.Text>
-                This is a description for the articles once done.
-            </Card.Text>
-            <Button className="me-2" variant="primary">Edit</Button>
-            <Button variant="danger">Delete</Button>
+                {/* Using the article passed down via the parameters, use them to populate the card */}
+                <Card.Title>{article.title}</Card.Title>
+                <Card.Subtitle>{article.creator}</Card.Subtitle>
+                <Card.Text>{article.content}</Card.Text>
+                <Card.Text>{moment(article.createdAt).fromNow()}</Card.Text>
+                <Button className="me-2" variant="primary">Edit</Button>
+                <Button variant="danger">Delete</Button>
             </Card.Body>
         </Card> 
     );
