98366e0b07df8a0733095a1d1f50f6f7efc9eace
Daniel Figueroa <danfigueroa81@gmail.com>
2022-11-16 21:00:44 -0500

    add icon and username to sidebar, change names

--- client/src/components/ArticleComponents/ArticleComponent.js
+++ client/src/components/ArticleComponents/ArticleComponent.js
@@ -10,6 +10,8 @@ const ArticleComponent = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
 
+  // tell React that your component needs to do something after render. React will remember the function you passed 
+  // (we’ll refer to it as our “effect”), and call it later after performing the DOM updates.
   useEffect(() => {
     dispatch(getArticles());
   }, [currentId, dispatch]);
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -8,6 +8,7 @@ import {
   CDBSidebarMenuItem,
 } from 'cdbreact';
 import { NavLink } from 'react-router-dom';
+import jinx from '../images/jinx.jpeg'
 
 const Sidebar = () => {
   return (
@@ -16,28 +17,42 @@ const Sidebar = () => {
         {/* Header - comes with a sandwich button that opens and closes it appears */}
         <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
           <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-            Sidebar
+            RCVV Backend Site
           </a>
         </CDBSidebarHeader>
 
         {/* Entries for the sidebar */}
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
+            <CDBSidebarMenuItem>
+              {/* Use React-Bootstrap grid system to properly handle rows and columns */}
+            <div className="d-flex justify-content-start flex-row mb-3">
+              <div className='flex-col'>
+                <img
+                  src={jinx}
+                  alt="bigfootjinx"
+                  style={{ width: '75px' }}
+                />
+              </div>
+              <div className="flex-col ms-2 d-flex align-items-center">Daniel Figueroa</div>
+            </div>
+            </CDBSidebarMenuItem>
+            {/* IMPORTANT FOR ICONS: The icons are from Font Awesome 5 */}
             <NavLink exact to="/" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">Home</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/articles" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Articles</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="edit">Articles</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/profile" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
+            <NavLink exact to="/newsletters" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="newspaper">Newsletters</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/analytics" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
+            <NavLink exact to="/volunteers" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="grin">Volunteers</CDBSidebarMenuItem>
             </NavLink>
 
-            <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="exclamation-circle">404 page</CDBSidebarMenuItem>
+            <NavLink exact to="/vendor" target="_blank" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="book">Vendor Items</CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
