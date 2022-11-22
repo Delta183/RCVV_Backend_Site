3e6e26461265d6694610785e24b7d5a877b672c9
Daniel Figueroa <danfigueroa81@gmail.com>
2022-11-22 09:42:47 -0500

    create two states for sign in

--- client/src/components/LoginComponents/LoginComponent.js
+++ client/src/components/LoginComponents/LoginComponent.js
@@ -18,6 +18,7 @@ const LoginComponent = () => {
     const [showPassword, setShowPassword] = useState(false);
     const handleShowPassword = () => setShowPassword(!showPassword);
 
+    // The two modes are sign up and sign in. This will flip them effectively
     const switchMode = () => {
         setForm(initialState);
         setIsSignup((prevIsSignup) => !prevIsSignup);
@@ -34,11 +35,6 @@ const LoginComponent = () => {
     }
     };
     
-    const clear = () => {
-        // setCurrentId(0);
-        // Be sure to set the attributes as none once more
-        setForm({ firstName: '', lastName: '', email: '', password: '', confirmPassword: '' });
-    };
 
     const googleSuccess = async (res) => {
     const result = res?.profileObj;
@@ -58,72 +54,77 @@ const LoginComponent = () => {
     const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
     return (
- <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-    <h2>Sign In</h2>
-    {/* First segment of the form (i.e. title, text input, extra text
-      mb refers to the level of the 'b' bottom margin. me is for the 'e' end margin  */}
-    <Form.Group className="m-3" name="title">
-      <Form.Label>First Name</Form.Label>
-      <Form.Control 
-      type="text" 
-      placeholder="Enter your first name..." 
-      value={form.firstName} 
-      // This is a setter method, the ...articleData in this case is necessary to ensure subsequent textfields don't override title
-      onChange={(e) => setForm({...form, firstName: e.target.value})}/>
-    </Form.Group>
+    <div className="">
+    <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <div className="d-flex justify-content-center">
+        {/* Make the correct title appear depending on which state it is */}
+        <h2>{ isSignup ? 'Sign up' : 'Sign in' }</h2>
+        </div>
+        {/* If signing up, display the name forms */}
+        { isSignup && (
+            <div>
+              {/* First segment of the form (i.e. title, text input, extra text
+        mb refers to the level of the 'b' bottom margin. me is for the 'e' end margin  */}
+                <Form.Group className="m-3" name="firstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control 
+                type="text" 
+                placeholder="Enter your first name..." 
+                // This is a setter method, the ...articleData in this case is necessary to ensure subsequent textfields don't override title
+                handleChange={handleChange}/>
+                </Form.Group>
 
-    {/* Second Segment */}
-    <Form.Group className="m-3" name="creator">
-      <Form.Label>Last Name</Form.Label>
-      <Form.Control 
-      type="text" 
-      placeholder="Enter your last name..." 
-      value={form.lastName}
-      onChange={(e) => setForm({...form, lastName: e.target.value})}
-      />
-    </Form.Group>
+                {/* Second Segment */}
+                <Form.Group className="m-3" name="lastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control 
+                type="text" 
+                placeholder="Enter your last name..." 
+                handleChange={handleChange}
+                />
+                </Form.Group>
+            </div>
+        )}
+       
 
-    {/* Third Segment */}
-    <Form.Group className="m-3" name="content">
-      <Form.Label>Email</Form.Label>
-      <Form.Control
-      type="email" 
-      placeholder="Enter your email..." 
-      value={form.email}
-      onChange={(e) => setForm({...form, email: e.target.value})}
-      />
-    </Form.Group>
-   
-      {/* Fourth Segment */}
-    <Form.Group className="m-3" name="content">
-      <Form.Label>Password</Form.Label>
-      <Form.Control
-      type="password" 
-      placeholder="Enter your password..." 
-      value={form.password}
-      onChange={(e) => setForm({...form, password: e.target.value})}
-      />
-    </Form.Group>
+        {/* Third Segment */}
+        <Form.Group className="m-3" name="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+        type="email" 
+        placeholder="Enter your email..." 
+        handleChange={handleChange}
+        />
+        </Form.Group>
+    
+        {/* Fourth Segment */}
+        <Form.Group className="m-3" name="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+        type="password" 
+        placeholder="Enter your password..." 
+        handleChange={handleChange}
+        />
+        </Form.Group>
 
-    {/* Fifth Segment */}
-    <Form.Group className="m-3" name="content">
-      <Form.Label>Confirm Password</Form.Label>
-      <Form.Control
-      type="password" 
-      placeholder="Confirm your password..." 
-      value={form.confirmPassword}
-      onChange={(e) => setForm({...form, confirmPassword: e.target.value})}
-      />
-    </Form.Group>
-
-    {/* Submit and clear button to end all information */}
-    <Button className="m-3" variant="primary" type="submit">
-      Submit
-    </Button>
-    <Button variant="primary" onClick={clear}>
-      Clear
-    </Button>
+        {/* Fifth Segment */}
+        <Form.Group className="m-3" name="confirmPassword">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control
+        type="password" 
+        placeholder="Confirm your password..." 
+        handleChange={handleChange}
+        />
+        </Form.Group>
+        {/* The sign in button and the next button reflect the current state of sign in*/}
+        <Button className="m-3" variant="primary" type="submit">
+        { isSignup ? 'Sign Up' : 'Sign In' }
+        </Button>
+        <Button className="m-3" variant="primary" onClick={switchMode}>
+        { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
+        </Button>
     </Form>
+    </div>
     );
 }
 
